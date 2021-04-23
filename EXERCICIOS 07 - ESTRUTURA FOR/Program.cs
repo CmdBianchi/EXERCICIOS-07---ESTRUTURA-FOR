578aec32d002e62d4b0e4bbe6b6a994101aca6de
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-23 17:07:46 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCICIOS 07 - ESTRUTURA FOR/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace EXERCICIOS_07___ESTRUTURA_FOR{
+    class Program{
+        /// -------> START / MAIN 
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Main(string[] args){
+            Msg();
+            Conditional();
+        }
+        /// -------> FUNCTIONS
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Msg(){
+            Console.WriteLine("Exercício 04 - estrutura repetitiva FOR - Lista de exercícios.");
+        }
+        //------------------------------------------------------------------------------------------//
+        static void Conditional(){
+            int n = int.Parse(Console.ReadLine()); //----> Números de casos
+
+              for (int i = 1; i <= n; i++){
+                int primeiro = i;
+                int segundo = i * i;
+                int terceiro = i * i * i;
+                Console.WriteLine($"{primeiro} {segundo} {terceiro}");
+              }           
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        /// -------> END
+    }
+}
